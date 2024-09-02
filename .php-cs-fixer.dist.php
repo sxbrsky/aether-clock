173a7580934b22c976f8aba479b2b47305631dfb
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-09-02 08:38:58 +0200

    chore: unify project files

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,55 +1,43 @@
-<?php
+<?php declare(strict_types=1);
 
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
+/*
+ * This file is part of the sxbrsky/clock.
+ *
+ * Copyright (C) 2024 Dominik Szamburski
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 
-$finder = Finder::create()
-  ->in([
-    __DIR__ . '/src',
-    __DIR__ . '/tests',
-  ])
-  ->name('*.php')
-  ->ignoreDotFiles(TRUE)
-  ->ignoreVCS(TRUE);
+$finder = \PhpCsFixer\Finder::create()
+    ->name('*.php')
+    ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true);
 
-return (new Config())
-  ->setRules([
-    '@PSR12' => TRUE,
-    'array_syntax' => ['syntax' => 'short'],
-    'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'no_unused_imports' => TRUE,
-    'not_operator_with_successor_space' => TRUE,
-    'trailing_comma_in_multiline' => TRUE,
-    'phpdoc_scalar' => TRUE,
-    'unary_operator_spaces' => TRUE,
-    'binary_operator_spaces' => TRUE,
-    'blank_line_before_statement' => [
-      'statements' => [
-        'break',
-        'continue',
-        'declare',
-        'return',
-        'throw',
-        'try',
-      ],
-    ],
-    'phpdoc_single_line_var_spacing' => TRUE,
-    'phpdoc_var_without_name' => TRUE,
-    'class_attributes_separation' => [
-      'elements' => [
-        'method' => 'one',
-      ],
-    ],
-    'braces' => [
-      'allow_single_line_closure' => TRUE,
-      'position_after_anonymous_constructs' => 'same',
-      'position_after_functions_and_oop_constructs' => 'next',
-      'position_after_control_structures' => 'same',
-    ],
-    'method_argument_space' => [
-      'on_multiline' => 'ensure_fully_multiline',
-      'keep_multiple_spaces_after_comma' => TRUE,
-    ],
-    'single_trait_insert_per_statement' => TRUE,
-  ])
-  ->setFinder($finder);
+$config = new \PhpCsFixer\Config();
+$config
+    ->setFinder($finder)
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'concat_space' => ['spacing' => 'one'],
+        'include' => true,
+        'new_with_braces' => true,
+        'no_empty_statement' => true,
+        'no_extra_blank_lines' => true,
+        'no_leading_import_slash' => true,
+        'no_leading_namespace_whitespace' => true,
+        'no_multiline_whitespace_around_double_arrow' => true,
+        'multiline_whitespace_before_semicolons' => true,
+        'no_singleline_whitespace_before_semicolons' => true,
+        'no_trailing_comma_in_singleline_array' => true,
+        'no_unused_imports' => true,
+        'no_whitespace_in_blank_line' => true,
+        'object_operator_without_whitespace' => true,
+        'ordered_imports' => true,
+        'standardize_not_equals' => true,
+        'ternary_operator_spaces' => true,
+    ]);
+
+return $config;
